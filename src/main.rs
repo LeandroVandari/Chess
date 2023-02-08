0fb7e8f7e3f905a0de08c83e0d1b02ee56e246e4
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-02-08 19:46:07 -0300

    Updated main to basic code, representing possible states.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+#[derive(Debug)]
+struct Board {
+    board: [Option<Piece>; 64]
+}
+
+impl Board {
+    fn empty() -> Board {
+        Board {
+            board: [None; 64]
+        }
+    }
+
+    fn new() -> Board {
+        let mut board = Self::empty();
+        board.board[0] = Some(Piece::new(PieceTypes::Rook, Color::White));
+        board
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum PieceTypes {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Color {
+    Black,
+    White
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Piece {
+    variant: PieceTypes,
+    color: Color
+}
+
+impl Piece {
+    fn new(variant: PieceTypes, color: Color) -> Piece {
+        Piece {variant, color}
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board::new();
+    println!("{:?}", board);
 }

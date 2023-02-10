abb188657dedd6861243d9a893cb3fd75b86f589
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-02-10 11:46:59 -0300

    Moved code to lib.rs and added benchmarking.

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,12 @@
+use chess::Board;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("instantiate_board", |b| b.iter(|| Board::new()));
+    c.bench_function("calculate_moves", |b| {
+        b.iter(|| Board::new().possible_movements())
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);

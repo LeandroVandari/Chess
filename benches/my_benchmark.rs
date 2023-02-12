ee43d241befd4227db433e576c02ed022553762b
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-02-12 12:23:43 -0300

    Auto commit.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -2,10 +2,9 @@ use chess::Board;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    let board = Board::new();
     c.bench_function("instantiate_board", |b| b.iter(|| Board::new()));
-    c.bench_function("calculate_moves", |b| {
-        b.iter(|| Board::new().possible_movements())
-    });
+    c.bench_function("calculate_moves", |b| b.iter(|| board.possible_movements()));
 }
 
 criterion_group!(benches, criterion_benchmark);

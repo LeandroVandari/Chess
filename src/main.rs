f4fd6605f011eb15d9ca029668d8e7b7425737cf
Leandro Vandari <bernardokreutz@gmail.com>
2023-02-21 21:58:07 -0300

    Deleted everything for code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use chess::{Board, Color, Piece, PieceTypes};
-
 fn main() {
-    /*     let board = Board::new();
-    let movements = board.possible_movements();
-    println!("{board}");
-    println!("{movements:?}"); */
-    Board::start_program();
-    let board = Board::example();
-
-    println!("{board}");
-    let movements = board.possible_movements();
-    println!("{movements:?}");
+    
 }

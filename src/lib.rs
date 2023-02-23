5916895eb52c17d9302f83a3e0f920c2949f8a48
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-02-23 20:38:47 -0300

    Auto commit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 use std::fmt;
 
-pub static UP: [Option<u8>; 64] = [None;64];
-pub static DOWN: [Option<u8>; 64] = [None;64];
-pub static LEFT: [Option<u8>; 64] = [None;64];
-pub static RIGHT: [Option<u8>; 64] = [None;64];
-pub static DOWN_RIGHT: [Option<u8>; 64] = [None;64];
-pub static DOWN_LEFT: [Option<u8>; 64] = [None;64];
-pub static UP_RIGHT: [Option<u8>; 64] = [None;64];
-pub static UP_LEFT: [Option<u8>; 64] = [None;64];
+// Pre-computed values for relative squares for each square.
+pub static UP: [Option<u8>; 64] = [Some(8), Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), Some(15), Some(16), Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), Some(23), Some(24), Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), Some(31), Some(32), Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), Some(39), Some(40), Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), Some(47), Some(48), Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), Some(55), Some(56), Some(57), Some(58), Some(59), Some(60), Some(61), Some(62), Some(63), None, None, None, None, None, None, None, None];
+pub static DOWN: [Option<u8>; 64] = [None, None, None, None, None, None, None, None, Some(0), Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), Some(7), Some(8), Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), Some(15), Some(16), Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), Some(23), Some(24), Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), Some(31), Some(32), Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), Some(39), Some(40), Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), Some(47), Some(48), Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), Some(55)];
+pub static LEFT: [Option<u8>; 64] = [None, Some(0), Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), None, Some(8), Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), None, Some(16), Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), None, Some(24), Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), None, Some(32), Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), None, Some(40), Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), None, Some(48), Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), None, Some(56), Some(57), Some(58), Some(59), Some(60), Some(61), Some(62)];
+pub static RIGHT: [Option<u8>; 64] = [Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), Some(7), None, Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), Some(15), None, Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), Some(23), None, Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), Some(31), None, Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), Some(39), None, Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), Some(47), None, Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), Some(55), None, Some(57), Some(58), Some(59), Some(60), Some(61), Some(62), Some(63), None];
+pub static DOWN_RIGHT: [Option<u8>; 64] = [None, None, None, None, None, None, None, None, Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), Some(7), None, Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), Some(15), None, Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), Some(23), None, Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), Some(31), None, Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), Some(39), None, Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), Some(47), None, Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), Some(55), None];
+pub static DOWN_LEFT: [Option<u8>; 64] = [None, None, None, None, None, None, None, None, None, Some(0), Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), None, Some(8), Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), None, Some(16), Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), None, Some(24), Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), None, Some(32), Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), None, Some(40), Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), None, Some(48), Some(49), Some(50), Some(51), Some(52), Some(53), Some(54)];
+pub static UP_RIGHT: [Option<u8>; 64] = [Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), Some(15), None, Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), Some(23), None, Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), Some(31), None, Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), Some(39), None, Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), Some(47), None, Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), Some(55), None, Some(57), Some(58), Some(59), Some(60), Some(61), Some(62), Some(63), None, None, None, None, None, None, None, None, None];
+pub static UP_LEFT: [Option<u8>; 64] = [None, Some(8), Some(9), Some(10), Some(11), Some(12), Some(13), Some(14), None, Some(16), Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), None, Some(24), Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), None, Some(32), Some(33), Some(34), Some(35), Some(36), Some(37), Some(38), None, Some(40), Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), None, Some(48), Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), None, Some(56), Some(57), Some(58), Some(59), Some(60), Some(61), Some(62), None, None, None, None, None, None, None, None];
+
+fn up(square: u8)
+
 
 // The board. Is wrapped in a struct in order to implement methods.
 pub struct Board {
@@ -106,7 +110,11 @@ pub struct Queen {color: Color}
 #[derive(Clone, Copy, Debug)]
 pub struct King {color: Color}
 
-
+impl PieceTrait for Pawn {
+    fn generate_moves(&self, square: u8, board: Board) -> Vec<u8> {
+        
+    }
+}
 
 
 // Print board to the terminal

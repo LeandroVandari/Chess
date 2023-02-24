97a7abee29caff5afd704998d69ffc637bdeaea2
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-02-24 17:12:23 -0300

    Separated board into a single file. Created pawn moves

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,11 +1,10 @@
-use chess::{Board};
+use chess::board::*;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut board = Board::example();
     c.bench_function("instantiate_board", |b| b.iter(|| Board::new()));
-/*     c.bench_function("pawn", |b| b.iter(|| board.board[9].unwrap().generate_moves())); */
-    
+    /*     c.bench_function("pawn", |b| b.iter(|| board.board[9].unwrap().generate_moves())); */
 }
 
 criterion_group!(benches, criterion_benchmark);
---
+++ src/board.rs
@@ -0,0 +1,238 @@
+use std::fmt;
+use super::{Piece, Color, Pawn, Knight, Bishop, Rook, Queen, King};
+
+// The board. Is wrapped in a struct in order to implement methods.
+pub struct Board {
+    pub board: [Option<Piece>; 64],
+}
+
+// functions that affect the board
+impl Board {
+    // return an empty board
+    fn empty() -> Self {
+        Board { board: [None; 64] }
+    }
+
+    // return a board in the starting chess position.
+    pub fn new() -> Self {
+        let mut board = Self::empty();
+        board.add_piece(
+            Piece::Rook(Rook {
+                color: Color::White,
+            }),
+            0,
+        );
+        board.add_piece(
+            Piece::Knight(Knight {
+                color: Color::White,
+            }),
+            1,
+        );
+        board.add_piece(
+            Piece::Bishop(Bishop {
+                color: Color::White,
+            }),
+            2,
+        );
+        board.add_piece(
+            Piece::Queen(Queen {
+                color: Color::White,
+            }),
+            3,
+        );
+        board.add_piece(
+            Piece::King(King {
+                color: Color::White,
+            }),
+            4,
+        );
+        board.add_piece(
+            Piece::Bishop(Bishop {
+                color: Color::White,
+            }),
+            5,
+        );
+        board.add_piece(
+            Piece::Knight(Knight {
+                color: Color::White,
+            }),
+            6,
+        );
+        board.add_piece(
+            Piece::Rook(Rook {
+                color: Color::White,
+            }),
+            7,
+        );
+        for i in 8..=15 {
+            board.add_piece(
+                Piece::Pawn(Pawn {
+                    color: Color::White,
+                }),
+                i,
+            );
+        }
+
+        board.add_piece(
+            Piece::Rook(Rook {
+                color: Color::Black,
+            }),
+            63,
+        );
+        board.add_piece(
+            Piece::Knight(Knight {
+                color: Color::Black,
+            }),
+            62,
+        );
+        board.add_piece(
+            Piece::Bishop(Bishop {
+                color: Color::Black,
+            }),
+            61,
+        );
+        board.add_piece(
+            Piece::Queen(Queen {
+                color: Color::Black,
+            }),
+            60,
+        );
+        board.add_piece(
+            Piece::King(King {
+                color: Color::Black,
+            }),
+            59,
+        );
+        board.add_piece(
+            Piece::Bishop(Bishop {
+                color: Color::Black,
+            }),
+            58,
+        );
+        board.add_piece(
+            Piece::Knight(Knight {
+                color: Color::Black,
+            }),
+            57,
+        );
+        board.add_piece(
+            Piece::Rook(Rook {
+                color: Color::Black,
+            }),
+            56,
+        );
+        for i in 48..=55 {
+            board.add_piece(
+                Piece::Pawn(Pawn {
+                    color: Color::Black,
+                }),
+                i,
+            );
+        }
+
+        board
+    }
+
+    pub fn example() -> Self {
+        let mut board = Self::empty();
+
+        board.add_piece(
+            Piece::Pawn(Pawn {
+                color: Color::White,
+            }),
+            9,
+        );
+        board.add_piece(
+            Piece::Rook(Rook {
+                color: Color::Black,
+            }),
+            18,
+        );
+        board.add_piece(
+            Piece::King(King {
+                color: Color::Black,
+            }),
+            17,
+        );
+        board.add_piece(
+            Piece::Knight(Knight {
+                color: Color::White,
+            }),
+            19,
+        );
+        board.add_piece(
+            Piece::King(King {
+                color: Color::White,
+            }),
+            63,
+        );
+        board.add_piece(
+            Piece::Queen(Queen {
+                color: Color::White,
+            }),
+            11,
+        );
+        board.add_piece(
+            Piece::Bishop(Bishop {
+                color: Color::Black,
+            }),
+            2,
+        );
+        board
+    }
+
+    // add a piece to a specific board location
+    fn add_piece(&mut self, piece: Piece, square_to_add_piece: usize) {
+        self.board[square_to_add_piece] = Some(piece);
+    }
+
+    pub fn get_row(square: u8) -> u8 {
+        square / 8
+    }
+    pub fn get_column(square: u8) -> u8 {
+        square % 8
+    }
+}
+
+// Print board to the terminal
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut board = String::new();
+        for (square_counter, square) in self.board.into_iter().enumerate() {
+            if let Some(piece) = square {
+                match piece {
+                    Piece::Pawn(piece) => {
+                        board.push(if piece.color.is_white() { '♙' } else { '♟' })
+                    }
+                    Piece::Knight(piece) => {
+                        board.push(if piece.color.is_white() { '♘' } else { '♞' })
+                    }
+                    Piece::Bishop(piece) => {
+                        board.push(if piece.color.is_white() { '♗' } else { '♝' })
+                    }
+                    Piece::Rook(piece) => {
+                        board.push(if piece.color.is_white() { '♖' } else { '♜' })
+                    }
+                    Piece::Queen(piece) => {
+                        board.push(if piece.color.is_white() { '♕' } else { '♛' })
+                    }
+                    Piece::King(piece) => {
+                        board.push(if piece.color.is_white() { '♔' } else { '♚' })
+                    }
+                }
+            } else {
+                board.push('.');
+            }
+            board.push(' ');
+            if square_counter % 8 == 7 {
+                board.push('\n');
+            }
+        }
+        write!(f, "{}", board.as_str())
+    }
+}
+impl Default for Board {
+    fn default() -> Self {
+        Self::new()
+    }
+}

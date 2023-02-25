1c982907a539fd818ab1590c957b08d0b991f376
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-02-25 17:38:52 -0300

    Auto commit.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -6,7 +6,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("instantiate_board", |b| b.iter(|| Board::new()));
     c.bench_function("pawn", |b| b.iter(|| board.board[9].unwrap().get_moves(&board.board, 9)));
     c.bench_function("knight", |b| b.iter(|| board.board[19].unwrap().get_moves(&board.board, 19)));
-    c.bench_function("bishop", |b| b.iter(|| board.board[2].unwrap().get_moves(&board.board, 2)));
+    c.bench_function("bishop", |b| b.iter(|| board.board[36].unwrap().get_moves(&board.board, 36)));
 
 }
 

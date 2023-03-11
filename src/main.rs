28835f1fe4872b6718e5f0ddc338d8b3de6bf81d
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-03-11 20:33:20 -0300

    Changed search function to use a HashSet, improved performance

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap};
+use std::{collections::{HashSet}};
 
 use chess::evaluate;
 
@@ -6,7 +6,7 @@ fn main() {
     let board = chess::Board::new();
     /*     let all_moves = board.generate_moves(chess::Color::White);
     println!("{all_moves:?}"); */
-    let mut positions = HashMap::new();
+    let mut positions = HashSet::new();
     evaluate(&board, 2, chess::Color::White, &mut positions);
 
     println!("{board}");

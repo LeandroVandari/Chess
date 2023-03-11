eecf748efb24e19de701110d25b3d0e92be5b9bf
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-03-11 17:46:49 -0300

    Just changed main branch depth to 2

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     /*     let all_moves = board.generate_moves(chess::Color::White);
     println!("{all_moves:?}"); */
     let mut positions = HashMap::new();
-    evaluate(&board, 5, chess::Color::White, &mut positions);
+    evaluate(&board, 2, chess::Color::White, &mut positions);
 
     println!("{board}");
 }

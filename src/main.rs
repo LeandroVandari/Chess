84fe6a5ac5ff078a3be9525e38204ee0de58e95a
LeandroVandari <leandrovandari@users.noreply.github.com>
2023-03-12 15:54:25 -0300

    added a new function for calculating moves, BUT THERE IS A FUCKING BUG THAT THE MOVES ARE NOT CORRECT AND I CANT FUCKING FIGURE IT OUT AAAAAAAA

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::collections::HashSet;
 
-use chess::evaluate;
+use chess::multi_thread_eval;
 
 fn main() {
-    let board = chess::Board::example();
+    let board = chess::Board::new();
     /*     let all_moves = board.generate_moves(chess::Color::White);
     println!("{all_moves:?}"); */
     let mut positions = HashSet::with_capacity(10000);
-
-    evaluate(&board, 1, chess::Color::Black, &mut positions);
+    let depth = 3;
+    multi_thread_eval(&board, depth, chess::Color::White, &mut positions);
+/*     let mut i = 0;
     positions.iter().for_each(|position| {
+        i += 1;
         let mut board = chess::Board::new();
         board.board = *position;
-        println!("{board}")
-    })
+        println!("{board}\n{i}")
+    }); */
 }
--- src/search.rs
+++ src/search.rs
@@ -1,25 +1,93 @@
-use crate::{Color, Piece};
-use std::collections::HashSet;
+use crate::{Color, Move, Piece};
+use std::collections::{HashMap, HashSet};
 
 use super::Board;
 
-pub fn evaluate(
+pub fn multi_thread_eval(
     board: &Board,
     depth: u8,
     start_color: Color,
     positions: &mut HashSet<[Option<Piece>; 64]>,
 ) {
     let moves = board.generate_moves(start_color);
-
+    let mut amount_of_moves = 0;
     if depth != 0 {
+
         for tuple in &moves {
             for sqr in tuple.1 {
                 let new_board = board.make_move(*tuple.0 as usize, *sqr, start_color);
+
                 if !positions.contains(&new_board.board) {
-                    evaluate(&new_board, depth - 1, start_color.reverse(), positions);
+                    let next_board_moves = new_board.generate_moves(start_color.reverse());
+                    if !is_check(
+                        &next_board_moves,
+                        if let Color::White = start_color {
+                            new_board.white_king_pos
+                        } else {
+                            new_board.black_king_pos
+                        },
+                    ) {
+                        amount_of_moves += evaluate(
+                            &new_board,
+                            depth - 1,
+                            start_color.reverse(),
+                            positions,
+                            &next_board_moves,
+                        );
+                    }
                 }
             }
         }
     }
+}
+
+fn evaluate(
+    board: &Board,
+    depth: u8,
+    start_color: Color,
+    positions: &mut HashSet<[Option<Piece>; 64]>,
+    moves: &HashMap<u8, Vec<Move>>,
+) -> i32{
+    let mut amount_of_moves = 0;
+    if depth != 0 {
+        for tuple in moves {
+            for sqr in tuple.1 {
+                let new_board = board.make_move(*tuple.0 as usize, *sqr, start_color);
+                if !positions.contains(&new_board.board) {
+                    let next_board_moves = board.generate_moves(start_color.reverse());
+                    if !is_check(
+                        &next_board_moves,
+                        if let Color::White = start_color {
+                            new_board.white_king_pos
+                        } else {
+                            new_board.black_king_pos
+                        },
+                    ) {
+                        amount_of_moves += 1;
+                        amount_of_moves += evaluate(
+                            &new_board,
+                            depth - 1,
+                            start_color.reverse(),
+                            positions,
+                            &next_board_moves,
+                        );
+                    }
+               }
+            }
+        }
+    }
     positions.insert(board.board);
+    amount_of_moves
+}
+
+fn is_check(moves: &HashMap<u8, Vec<Move>>, king_pos: u8) -> bool {
+    moves.iter().any(|tuple| {
+        tuple.1.iter().any(|end_square| {
+            if let Move::RegularMove(a_square) = end_square {
+                *a_square == king_pos
+            } else {
+                false
+            }
+        })
+    })
 }

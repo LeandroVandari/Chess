f5ee3d4768549693b8dd1fe7b666bc8f4a568b8f
Leandro Vandari <bernardokreutz@gmail.com>
2023-03-14 22:05:40 -0300

    Continued trying to fix the bug

--- src/search.rs
+++ src/search.rs
@@ -1,5 +1,5 @@
-use crate::{Color, Move, Piece};
-use std::collections::{HashMap, HashSet};
+use crate::{convert_to_square, Color, Move, Piece};
+use std::collections::HashMap;
 
 use super::Board;
 
@@ -7,35 +7,41 @@ pub fn multi_thread_eval(
     board: &Board,
     depth: u8,
     start_color: Color,
-    positions: &mut HashSet<[Option<Piece>; 64]>,
+    positions: &mut HashMap<[Option<Piece>; 64], i32>,
 ) {
     let moves = board.generate_moves(start_color);
     let mut amount_of_moves = 0;
+    let mut moves_each_tree: i32;
     if depth != 0 {
         for tuple in &moves {
             for each_move in tuple.1 {
                 let new_board = board.make_move(*tuple.0 as usize, *each_move, start_color);
 
-                if !positions.contains(&new_board.board) {
-                    let next_board_moves = new_board.generate_moves(start_color.reverse());
-                    if !is_check(
+                //if !positions.contains_key(&new_board.board) {
+                let next_board_moves = new_board.generate_moves(start_color.reverse());
+                if !is_check(
+                    &next_board_moves,
+                    if let Color::White = start_color {
+                        new_board.white_king_pos
+                    } else {
+                        new_board.black_king_pos
+                    },
+                ) {
+                    let a = convert_to_square(*tuple.0);
+                    moves_each_tree = 0;
+                    evaluate(
+                        &new_board,
+                        depth - 1,
+                        start_color.reverse(),
+                        positions,
                         &next_board_moves,
-                        if let Color::White = start_color {
-                            new_board.white_king_pos
-                        } else {
-                            new_board.black_king_pos
-                        },
-                    ) {
-                        evaluate(
-                            &new_board,
-                            depth - 1,
-                            start_color.reverse(),
-                            positions,
-                            &next_board_moves,
-                            &mut amount_of_moves,
-                        );
-                    }
+                        &mut moves_each_tree,
+                    );
+
+                    println!("{a}{each_move}: {moves_each_tree}");
+                    amount_of_moves += moves_each_tree;
                 }
+                //}
             }
         }
     }
@@ -46,7 +52,7 @@ fn evaluate(
     board: &Board,
     depth: u8,
     start_color: Color,
-    positions: &mut HashSet<[Option<Piece>; 64]>,
+    positions: &mut HashMap<[Option<Piece>; 64], i32>,
     moves: &HashMap<u8, Vec<Move>>,
     amount_of_moves: &mut i32,
 ) {
@@ -54,7 +60,7 @@ fn evaluate(
         for tuple in moves {
             for each_move in tuple.1 {
                 let new_board = board.make_move(*tuple.0 as usize, *each_move, start_color);
-                // if !positions.contains(&new_board.board) {
+                //if !positions.contains_key(&new_board.board) {
                 let next_board_moves = board.generate_moves(start_color.reverse());
                 if !is_check(
                     &next_board_moves,
@@ -79,7 +85,7 @@ fn evaluate(
     } else {
         *amount_of_moves += 1;
     }
-    positions.insert(board.board);
+    positions.insert(board.board, *amount_of_moves);
 }
 
 fn is_check(moves: &HashMap<u8, Vec<Move>>, king_pos: u8) -> bool {

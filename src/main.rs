af6382db5470f9a5028df9aee2ba6d05b9506eef
Leandro Vandari <bernardokreutz@gmail.com>
2023-03-14 22:05:40 -0300

    Continued trying to fix the bug

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use chess::multi_thread_eval;
 
 fn main() {
-    let board = chess::Board::new();
-    /*     let all_moves = board.generate_moves(chess::Color::White);
-    println!("{all_moves:?}"); */
-    let mut positions = HashSet::new();
+    let board = chess::Board::new(); //.make_move(9, chess::Move::PawnAdvanceTwoSquares(25), chess::Color::White);
+                                     /*     let all_moves = board.generate_moves(chess::Color::White);
+                                     println!("{all_moves:?}"); */
+    let mut positions = HashMap::new();
     let depth = 3;
     multi_thread_eval(&board, depth, chess::Color::White, &mut positions);
     /*     let mut i = 0;

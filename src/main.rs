f0a2dab3ac32a59e2ba01f588753fc386a27cef7
Leandro Vandari <bernardokreutz@gmail.com>
2023-05-06 16:28:09 -0300

    Prepared for benchmarking

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     let board = chess::Board::new();
     println!("{board}");
     let mut positions = FnvHashSet::default();
-    let depth = 7;
+    let depth = 3;
     multi_thread_eval(&board, depth, chess::Color::White, &mut positions);
 }

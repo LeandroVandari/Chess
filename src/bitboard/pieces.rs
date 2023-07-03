7775e04aa8e46e3fa022750bbde53e2fdc6a7416
Leandro Vandari <bernardokreutz@gmail.com>
2023-07-02 22:04:03 -0300

    Hopefully made movements in lines work

--- src/bitboard/pieces.rs
+++ src/bitboard/pieces.rs
@@ -1,5 +1,7 @@
-pub trait Piece {
-    fn generate_moves(&self) -> u64;
+crate::bitboard::implement_bitboard!(Pawn, Knight, Bishop, Rook, Queen, King);
+
+pub trait Piece: super::BitBoard {
+    fn generate_moves(&self, own_side: u64, other_side: u64) -> u64;
 }
 pub enum PieceTypes {
     Pawn,
@@ -7,7 +9,7 @@ pub enum PieceTypes {
     Bishop,
     Rook,
     Queen,
-    King
+    King,
 }
 
 pub struct Pawn(pub u64);
@@ -17,9 +19,9 @@ pub struct Rook(pub u64);
 pub struct Queen(pub u64);
 pub struct King(pub u64);
 
-impl super::BitBoard for Pawn{}
-impl super::BitBoard for Bishop{}
-impl super::BitBoard for Knight{}
-impl super::BitBoard for Rook{}
-impl super::BitBoard for Queen{}
-impl super::BitBoard for King{}
+impl Piece for Rook {
+    fn generate_moves(&self, own_side: u64, other_side: u64) -> u64 {
+        let pc = self.0;
+        crate::bitboard::macros::move_in_line!(piece bitboard: pc, own side: own_side, opponent side: other_side, directions and conditions: [(1, crate::bitboard::consts::H_FILE, crate::bitboard::consts::A_FILE), (8, crate::bitboard::consts::LAST_ROW, crate::bitboard::consts::FIRST_ROW)])
+    }
+}

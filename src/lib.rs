feb46c47792bc16f2a8dbf9598fa195852f08c79
Leandro Vandari <bernardokreutz@gmail.com>
2023-10-02 22:09:29 -0300

    Added move making, and prepared for conversion from move into list of possible positions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![warn(clippy::pedantic)]
 //! This crate has the purpose of fully simulating the game of chess, including an engine to play against (WIP). Currently, for means of speed, the crate is using `bitboards` for simulating the game. All basic simulation can be done through the [bitboard] module.
 
 /// This module contains the game base, such as the [`Position`](bitboard::Position), moves etc.
